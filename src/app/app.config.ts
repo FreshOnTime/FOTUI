e4a38ad5f6124bcbae3d71c0effd8924d4b14196
ifs-bhdalk <bhanuka.dassanayake@ifs.com>
2024-12-21 02:35:26 +0530

    Changed Project ID from 'FOTUI' to 'fotui', Add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +19,6 @@ export const appConfig: ApplicationConfig = {
           darkModeSelector: '.my-app-dark',
         },
       },
-    }),
+    }), provideFirebaseApp(() => initializeApp({"projectId":"fresh-on-time","appId":"1:722952706056:web:f704b7149f1153dd9959bd","storageBucket":"fresh-on-time.firebasestorage.app","apiKey":"AIzaSyCdAHSAMh5fq8N8CzAF7IqYPAxULwzDaPU","authDomain":"fresh-on-time.firebaseapp.com","messagingSenderId":"722952706056","measurementId":"G-XDJR7RJCB2"})), provideAuth(() => getAuth()),
   ],
 };
