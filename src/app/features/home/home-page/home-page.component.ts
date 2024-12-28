13dd289842ef57498461cd90d5f6b1c15a2a4caa
Bhanuka Dassanayake <131243091+BhanukaDev@users.noreply.github.com>
2024-12-28 15:53:22 +0530

    Home page finished

--- src/app/features/home/home-page/home-page.component.ts
+++ src/app/features/home/home-page/home-page.component.ts
@@ -3,10 +3,12 @@ import { ProductCardComponent } from '../../product/product-card/product-card.co
 
 import { SplitButton } from 'primeng/splitbutton';
 import { MenuItem } from 'primeng/api';
+import { ProductsCarouselComponent } from '../../product/products-carousel/products-carousel.component';
+import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ProductsCarouselComponent, CarouselModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
